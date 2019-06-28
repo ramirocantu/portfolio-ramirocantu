8e79733b435b6406389ff8b626d7101004893a31
Ramiro Ivan Cantu <ramiro.cantu.ramirez@gmail.com>
2019-06-28 14:23:11 -0500

    update education info

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -36,12 +36,8 @@ const Education = () => (
           </p>
 
           <p>
-            This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel
-            velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum
-            auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.
-            Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
-            Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
-            ornare odio. Sed non mauris vitae erat
+            I've audited a boat load of courses in all things from react/javascript to computer vision and machine learning.
+            I think online learning has a lot of merrit to anyone willing to learn.
           </p>
         </div>
       </div>
